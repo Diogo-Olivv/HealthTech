24281eb36c0a92d12438dc15744d0bbedc7d6fde
Diih <diogocorjesu@gmail.com>
2026-05-25 12:30:38 -0300

    feat: Ambiente de Desenvolvimento Inicial Configurado

---
+++ backend/src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST', 'localhost'),
+        port: config.get<number>('DB_PORT', 5432),
+        username: config.get('DB_USER', 'postgres'),
+        password: config.get('DB_PASSWORD', 'postgres'),
+        database: config.get('DB_NAME', 'healthtech'),
+        entities: [User],
+        synchronize: config.get('NODE_ENV') !== 'production',
+      }),
+    }),
+    UsersModule,
+  ],
+})
+export class AppModule {}
