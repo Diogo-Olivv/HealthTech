8fc967d78bb35053c5fdf8c89544629776753ce1
martinnho <melomartin138@gmail.com>
2026-06-07 13:11:21 -0300

    refactor(dto): reduzir CreateUserDto aos campos comuns

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,53 +1,8 @@
-import { Type } from 'class-transformer';
-import {
-  IsDate,
-  IsEmail,
-  IsEnum,
-  IsString,
-  MinLength,
-  ValidateIf,
-  ValidateNested,
-} from 'class-validator';
-import { UserType } from '../../entities/user.entity';
-
-export class CreatePacienteDto {
-  @IsString()
-  cpf!: string;
-
-  @IsDate()
-  @Type(() => Date)
-  dataNascimento!: Date;
-}
-
-export class CreateMedicoDto {
-  @IsString()
-  crm!: string;
-
-  @IsString()
-  especialidade!: string;
-}
+import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
-  @IsString()
-  name!: string;
-
-  @IsEmail()
-  email!: string;
-
-  @IsString()
-  @MinLength(8)
-  password!: string;
-
-  @IsEnum(UserType)
-  tipo!: UserType;
-
-  @ValidateIf((o: CreateUserDto) => o.tipo === UserType.PACIENTE)
-  @ValidateNested()
-  @Type(() => CreatePacienteDto)
-  paciente?: CreatePacienteDto;
-
-  @ValidateIf((o: CreateUserDto) => o.tipo === UserType.MEDICO)
-  @ValidateNested()
-  @Type(() => CreateMedicoDto)
-  medico?: CreateMedicoDto;
+  @IsString() name!: string;
+  @IsEmail() email!: string;
+  @IsString() @MinLength(8) password!: string;
 }
+
