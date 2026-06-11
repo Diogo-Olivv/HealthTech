e9d286f4d8be157fc23ecb4ef09cfbf690c639a8
Hugo Rosa <194843128+HugoRosa29@users.noreply.github.com>
2026-06-11 19:12:58 -0300

    (feat)Modulo Medico Paciente Adicionado

---
+++ backend/src/medico-paciente/medico-paciente.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
+import { Reflector } from '@nestjs/core';
+import { MedicoPaciente } from '../entities/MedicoPaciente.entity';
+import { Medico } from '../entities/medico.entity';
+import { Paciente } from '../entities/paciente.entity';
+import { RolesGuard } from '../auth/roles.guard';
+import { MedicoPacienteController } from './medico-paciente.controller';
+import { MedicoPacienteService } from './medico-paciente.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([MedicoPaciente, Medico, Paciente]),
+    PassportModule,
+  ],
+  controllers: [MedicoPacienteController],
+  providers: [MedicoPacienteService, Reflector, RolesGuard],
+})
+export class MedicoPacienteModule {}
