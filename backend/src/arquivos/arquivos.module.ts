328bf809250299477a9ee41223a0939efdd304a5
Hugo Rosa <hugosousa9202@gmail.com>
2026-06-16 11:00:11 -0300

    (feat) Registrados MedicoPaciente, PassportModule, RolesGuard

--- backend/src/arquivos/arquivos.module.ts
+++ backend/src/arquivos/arquivos.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
+import { Reflector } from '@nestjs/core';
 import { Arquivo } from '../entities/arquivo.entity';
+import { MedicoPaciente } from '../entities/medico-paciente.entity';
+import { RolesGuard } from '../auth/roles.guard';
 import { ArquivosController } from './arquivos.controller';
 import { ArquivosService } from './arquivos.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Arquivo])],
+  imports: [
+    TypeOrmModule.forFeature([Arquivo, MedicoPaciente]),
+    PassportModule,
+  ],
   controllers: [ArquivosController],
-  providers: [ArquivosService],
+  providers: [ArquivosService, Reflector, RolesGuard],
   exports: [ArquivosService],
 })
 export class ArquivosModule {}
