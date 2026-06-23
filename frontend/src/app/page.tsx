fc3c5482ede54889e405183841eabaf7be74fbd7
lucaspaulaleal <lucaslplbr@gmail.com>
2026-06-23 12:43:03 -0300

    refactor: exclui linhas comentadas desnecessarias

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,23 +3,7 @@ import styles from './page.module.css';
 
 export default function Home() {
   return (
-    // <main>
-    //   <div >
-    //      <h1 >HealthTech</h1>
-    //      <p >
-    //        Bem-vindo! Acesse sua conta ou crie um novo cadastro para começar.
-    //      </p>
 
-    //      <div >
-    //        <Link href="/login" >
-    //          Entrar
-    //        </Link>
-    //        <Link href="/register" >
-    //          Criar conta
-    //        </Link>
-    //      </div>
-    //    </div>
-    // </main>
     <main className={styles.page}>
       <div className={styles.card}>
         <img src="/Icon.svg" alt="Logo HealthTech" className={styles.logo} />
